2772e4211983f96efe233d1b099d5e2cfa18ed19
derailed <fernand.galiana@gmail.com>
2017-09-30 11:41:53 -0600

    updates

--- got-ui/src/app/minion/minion.component.ts
+++ got-ui/src/app/minion/minion.component.ts
@@ -1,39 +1,40 @@
 import { Component, Input } from '@angular/core'
-import { Minion }           from '../shared/minion'
-import { Castle }           from '../shared/castle'
-import { ChannelService }   from '../channel.service'
+import { Minion } from '../shared/minion'
+import { Castle } from '../shared/castle'
+import { ChannelService } from '../channel.service'
 
 @Component({
-  selector:    'app-minion',
+  selector: 'app-minion',
   templateUrl: 'minion.component.html',
-  styleUrls:   [ 'minion.component.css' ]
+  styleUrls: ['minion.component.css']
 })
 export class MinionComponent {
-  @Input() 
+  @Input()
   minion: Minion
-  @Input() 
+  @Input()
   castle: Castle
-  
+
   selected: boolean
   glossary: any
-  
+
   constructor(private svc: ChannelService) {
     this.glossary = {
       cb: "Castle Black",
       rk: "Red Keep",
       ds: "DragonStone",
+      b: "Baretheon",
     }
   }
-  
+
   toggleVisible(flag: boolean) {
     this.selected = flag
   }
-  
+
   sendMissive(text: string) {
     const tokens = text.split("|")
     const payload = {
-      from: this.minion.name, 
-      body: tokens[1], 
+      from: this.minion.name,
+      body: tokens[1],
       at: Date.now()
     }
     this.svc.push(this.glossary[tokens[0]], payload)
